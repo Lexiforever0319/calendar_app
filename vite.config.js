2d75d3b95e0c95a740abf5ffbe0cc5634b447154
Lavender0319 <101381680+Lavender0319@users.noreply.github.com>
2026-03-07 08:54:09 +1100

    set base for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/calendar_app/',   // 这里改成你的仓库名，比如 '/myapp/'
 })
